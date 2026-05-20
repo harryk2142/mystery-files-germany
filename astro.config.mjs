52d4bf1b1bd6beb09d63b7314f69998b09d59d51
harryk2142 <>
2026-05-20 16:59:37 +0200

    add packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-check
 
 import mdx from "@astrojs/mdx";
+import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://the-ai-files.de",
-    integrations: [mdx(), sitemap()],
+    integrations: [mdx(), sitemap(), preact()],
+    server: {
+        port: 3000,
+    },
     fonts: [
         {
             provider: fontProviders.local(),
